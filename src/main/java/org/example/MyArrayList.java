a56409aa35b07492bcb168eb32984859515cecab
SavushkinSV <savushkin.sv@yandex.ru>
2024-08-31 18:12:01 +0300

    feat: add class MyArrayList

---
+++ src/main/java/org/example/MyArrayList.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * Собственная реализация ArrayList
+ * @author SavushkinSV
+ */
+public class MyArrayList {
+    private final int size = 10;
+    private Object[] array = new Object[size];
+
+}
