2be5c03058359da3b8978c6d711d10d6eeb36c4d
SavushkinSV <savushkin.sv@yandex.ru>
2024-08-31 19:04:17 +0300

    feat: add constructors & size()

--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,11 +1,69 @@
 package org.example;
 
 /**
- * Собственная реализация ArrayList
+ * Собственная реализация ArrayList.
+ *
  * @author SavushkinSV
  */
-public class MyArrayList {
-    private final int size = 10;
-    private Object[] array = new Object[size];
+public class MyArrayList<T> {
+
+    /**
+     * Емкость массива при инициализации.
+     */
+    private static final int INIT_CAPACITY = 10;
+
+    /**
+     * Размер массива.
+     */
+    private int size;
+
+    /**
+     * Емкость внутреннего массива.
+     */
+    private final int capacity;
+
+    /**
+     * Массив с данными.
+     */
+    private final Object[] array;
+
+    /**
+     * Конструктор без аргументов.
+     * Создает массив с количеством элементов по умолчанию.
+     */
+    public MyArrayList() {
+        this.capacity = INIT_CAPACITY;
+        this.array = new Object[capacity];
+    }
+
+    /**
+     * @param capacity емкость создаваемого массива
+     */
+    public MyArrayList(int capacity) {
+        if (capacity >= 0) {
+            this.capacity = capacity;
+            this.array = new Object[capacity];
+        } else {
+            throw new IllegalArgumentException("Некорректное значение массива:" + capacity);
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Добавляет новый элемент в массив.
+     * При достижении максимального значения внутреннего массива удваивает его.
+     *
+     * @param item
+     */
+    public void add(T item) {
+        array[size++] = item;
+    }
+
+    private void resize() {
+
+    }
 
 }
