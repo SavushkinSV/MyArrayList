6751a58940bc8eb08277df9ab97a62b1b98312cf
Savushkin Sergey <savushkinsv@mail.ru>
2024-09-03 09:15:28 +0300

    feat: add tests for check capacity

--- src/test/java/MyArrayListTest.java
+++ src/test/java/MyArrayListTest.java
@@ -1,8 +1,19 @@
 import org.example.MyArrayList;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 public class MyArrayListTest {
+
+    public static MyArrayList<String> stringList;
+
+    @BeforeAll
+    public static void init() {
+        stringList = new MyArrayList<>(Arrays.asList("Moscow", "Kazan", "Yaroslavl", "Novosibirsk", "Murmansk"));
+    }
 
     @Test
     public void testNewMyArrayListShouldHaveSizeZero() {
@@ -14,7 +25,8 @@ public class MyArrayListTest {
     @Test
     public void testExpectedExceptionWithNegativeArrayValue() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new MyArrayList<>(-10);});
+            new MyArrayList<>(-10);
+        });
 
         Assertions.assertEquals("Некорректное значение массива: -10", exception.getMessage());
     }
@@ -40,6 +52,12 @@ public class MyArrayListTest {
     }
 
     @Test
+    public void testAddMyArrayListShouldHaveSizeFive() {
+
+        Assertions.assertEquals(5, stringList.size());
+    }
+
+    @Test
     public void testGetShouldReturnString() {
         MyArrayList<String> list = new MyArrayList<>();
         list.add("Hello");
@@ -60,6 +78,37 @@ public class MyArrayListTest {
         });
 
         Assertions.assertEquals("Некорректный индекс элемента массива: 2", exception.getMessage());
+    }
+
+    @Test
+    public void testNewMyArrayListShouldHaveCapacityTen() {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        try {
+            Field field = field = list.getClass().getDeclaredField("capacity");
+            field.setAccessible(true);
+            int capacity = (int) field.get(list);
+
+            Assertions.assertEquals(10, capacity);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testAddManyObjectShouldIncreaseCapacity() {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        for (int i = 0; i <= 10; i++) {
+            list.add(i);
+        }
+        try {
+            Field field = field = list.getClass().getDeclaredField("capacity");
+            field.setAccessible(true);
+            int capacity = (int) field.get(list);
+
+            Assertions.assertEquals(20, capacity);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
